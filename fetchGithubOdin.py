dd7994de2569a60ea9492851063ea31cca640d96
MvBonin <markwartbonin@hotmail.com>
2021-05-10 13:47:40 +0200

    Added notification and last Updated

--- fetchGithubOdin.py
+++ fetchGithubOdin.py
@@ -1,5 +1,6 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from notify import notification
 
 content = urllib.request.urlopen('https://github.com/orgs/elementary/projects/55')
 
@@ -8,5 +9,9 @@ read_content = content.read()
 soup = BeautifulSoup(read_content, 'html.parser')
 
 tooltip = soup.find(class_ = "tooltipped tooltipped-s")
+lastUpdated = soup.find("relative-time", class_ = "no-wrap")
 if tooltip:
-    print(tooltip['aria-label'])
+    
+    sendString =  tooltip['aria-label'] + " - Updated " + lastUpdated.text
+    print(sendString)
+    notification(sendString, title='eOS Odin Dev Status')
