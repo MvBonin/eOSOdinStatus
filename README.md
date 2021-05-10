24486afbb982fd7f459e3eb04fbff6fabfd57367
MvBonin <markwartbonin@hotmail.com>
2021-05-10 14:34:26 +0200

    updated Readme and added tempfile

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # eOSOdinStatus
+A simple python script to notify about progress on elementary OS 6.0 Odin Release.
+
+##Usage
+Just run the script with python3 fetchGithubOdin.py
+or with 
+python3 fetchGithubOdin.py -n
+if you want to get a notification, even if the State of eOS has not changed since last time you checked.
+
+I just run it hourly with cron:
+sudo crontab -e
+-- add a new line:
+0 * * * * /path/to/script/fetchGithubOdin.py
+##Requirements
+pip3 install urllib
+pip3 install notify-send
+pip3 install bs4
--- fetchGithubOdin.py
+++ fetchGithubOdin.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
 import urllib.request
 from bs4 import BeautifulSoup
 from notify import notification
+import sys
+
+## Is the Notification arg set? -n
+isNotificationArg = False
+if len(sys.argv) > 1:
+    if sys.argv[1] == "-n":
+        isNotificationArg = True
+
 
 content = urllib.request.urlopen('https://github.com/orgs/elementary/projects/55')
 
@@ -10,8 +19,26 @@ soup = BeautifulSoup(read_content, 'html.parser')
 
 tooltip = soup.find(class_ = "tooltipped tooltipped-s")
 lastUpdated = soup.find("relative-time", class_ = "no-wrap")
-if tooltip:
+sendString =  tooltip['aria-label'] + " - Updated " + lastUpdated.text
+lastRead = ""
+
+try:
+    f = open("lastStatus", "r")
+except IOError:
+    print("not openable")
+    g = open("lastStatus", "x")
+    g.write(sendString)
+    isNotificationArg = True
+    g.close()
+
+
+with open("lastStatus", "r") as f:
+    lastRead = f.read()
+
+
+if lastRead != sendString or isNotificationArg:
+    print("Status Notification")
+    notification(sendString, title='eOS 6.0 Odin Dev Status', app_name="elementaryOS", image="computer")
     
-    sendString =  tooltip['aria-label'] + " - Updated " + lastUpdated.text
-    print(sendString)
-    notification(sendString, title='eOS Odin Dev Status')
+print(sendString)
+
